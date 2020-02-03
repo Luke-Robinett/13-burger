2c0db47b4a49bf93c58d5eb48803ecf712324c31
Luke Robinett <lukebootcamp@gmail.com>
2020-02-03 15:40:50 -0800

    Updated connection.js file for compatibility with JawsDB.

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection(
- {
-  host: "localhost",
-  user: 'luke',
-  password: 'bootcamp',
-  database: 'burger_db'
- }
-);
+var connection;
+
+if (process.env.JAWSDB_URL) {
+ connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+ connection = mysql.createConnection(
+  {
+   host: "localhost",
+   user: 'luke',
+   password: 'bootcamp',
+   database: 'burger_db'
+  }
+ );
+}
 
 connection.connect(err => {
  if (err) throw err;
