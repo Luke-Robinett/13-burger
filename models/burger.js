6a2fce1edd7f31e9504e51c655dd7db1a821cd1f
Luke Robinett <lukebootcamp@gmail.com>
2020-02-02 22:17:58 -0800

    Created data model

---
+++ models/burger.js
@@ -0,0 +1,21 @@
+const orm = require("./../config/orm");
+
+const burger = {
+ getAll: cb => {
+  orm.selectAll(result => {
+   cb(result);
+  });
+ },
+ add: (name, cb) => {
+  orm.insertOne(name, result => {
+   cb(result);
+  });
+ },
+ devour: (id, cb) => {
+  orm.updateOne(id, result => {
+   cb(result);
+  });
+ }
+};
+
+module.exports = burger;
